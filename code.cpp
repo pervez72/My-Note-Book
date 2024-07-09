f86df343c807d47ac7c8b90e99c54f88b445cba5
pervez <phparvez71@gmail.com>
2024-07-09 21:36:20 +0600

    ok

---
+++ code.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[26]={0};
+    int size;
+    cin>>size;
+
+    for (int i=0; i<size; i++)
+    {
+        char c;
+        cin>>c;
+        arr[]
+    }
+    
+    
+
+
+}
