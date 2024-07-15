93a7e7aae59bd4d169aa831ee20331a3a0e8a6f6
pervez <phparvez71@gmail.com>
2024-07-15 17:47:52 +0600

    ok

---
+++ code.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+
+    for (int i=0; i<s.size(); i++)
+    {
+        for (int j=i+1; j<s.size(); j++)
+        {
+            sort()
+        }
+        
+    }
+    cout<<
+}
