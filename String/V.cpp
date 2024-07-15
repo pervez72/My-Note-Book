a40744ebf5411b4837c402eb36e7b2fdd7fae9fb
pervez <phparvez71@gmail.com>
2024-07-15 12:12:28 +0600

    v done

--- String/V.cpp
+++ String/V.cpp
@@ -3,44 +3,17 @@ using namespace std;
 
 int main(){
 
-  string s;
-  cin>>s;
-  int e=0,g=0,p=0,y=0,t=0;
-  int size=s.size();
+    string s;
+    cin>>s;
 
-  for (int i = 0; i < size; i++)
-  {
-    if (s[i]=='e' ||s[i]=='E')
-    {
-     e++;
+    for (int i = 0; i < s.size(); i++)
+    {   
+        if (s[i]=='E' && s[i+1]=='G' && s[i+2]=='Y' && s[i+3]=='P' && s[i+4]=='T')
+        {
+            i=i+4; //this line skip gypt
+            cout<<" ";
+        }
+        else
+            cout<<s[i];
     }
-    if (s[i]=='g' ||s[i]=='G')
-    {
-     g++;
-    }
-    if (s[i]=='p' ||s[i]=='P')
-    {
-     p++;
-    }
-    if (s[i]=='y' ||s[i]=='Y')
-    {
-     y++;
-    }
-    if (s[i]=='t' ||s[i]=='T')
-    {
-     t++;
-    }
-  }
-
-  int m1=min(e,g);
-  int m2=min(y,p);
-  int m3=min(t,e);
-
-  int m4=min(m1,m2);
-  int m5=min(m2,m3);
-
-  int m6=min(m4,m5);
-  cout<<m6;
-
-  
 }
