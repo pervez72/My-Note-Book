e66fe8d05238a65b0f8218570749e77463c6c11b
pervez <phparvez71@gmail.com>
2024-07-20 12:17:18 +0600

    ok

---
+++ G.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/// @brief 
+void max_min(){
+int a;
+}
+
+
+
+
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+
+    if (n>=1 && n<=1000)
+    {
+        for (int i=0; i<n; i++)
+        {
+            cin>>arr[i];
+        }
+    }
+    max_min(); 
+}
