eea2a8b8668343a758e8b55647a0d70949b9a3f4
pervez <phparvez71@gmail.com>
2024-10-16 21:04:33 +0600

    all

---
+++ Accademic/conditon.py
@@ -0,0 +1,23 @@
+#Conditional statement
+x=10
+if x!=10:
+    print("if condition is true")
+elif x==10:
+   print("elif condition is true")
+else:
+    print("else condition is trure")
+
+print("\n")
+#ternary operator 
+#value_if_true if condition else value_if_false
+x = 8
+print("x is even") if x % 2 == 0 else print("x is odd")
+
+age = 18
+status = "Adult" if age >= 18 else "Minor"
+print(status)
+
+x = 10
+result = "Even" if x % 2 == 0 else "Odd"
+print(result)
+
