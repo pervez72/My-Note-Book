ce5ad78af833ffd0dfe1832e06a1d41a9b2c0ce8
pervez <phparvez71@gmail.com>
2024-10-16 21:53:03 +0600

    list set and tuple

---
+++ python/Accademic/Touple.py
@@ -0,0 +1,11 @@
+# Tuple definition
+my_tuple = (1, 2, 3)
+
+# Access elements
+print(my_tuple[2])
+
+#right way print 
+print(my_tuple)
+
+# You cannot modify the elements of a tuple
+# my_tuple[1] = 10  # This will cause an error
