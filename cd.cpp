fec4c9d7fb3bb1fd3819b6f27915d4c1e197c3b9
pervez <phparvez71@gmail.com>
2024-10-23 21:38:42 +0600

    ok

--- cd.cpp
+++ cd.cpp
@@ -3,5 +3,28 @@ using namespace std;
 
 int main(){
    
-    sds
+    int n,c;
+    cin>>n>>c;
+
+    int a[n];
+    for (int i = 0; i<n; i++)
+    {
+       cin>>a[i];
+    }
+    
+    int cnt=1;
+    for (int i=0; i < n; i++)
+    {   
+        if(a[0]){
+            cnt++;
+        }
+
+        if ((a[i+1]-a[i])>=c)
+        {
+            cnt++;
+
+        }
+        
+    }
+    
 }
